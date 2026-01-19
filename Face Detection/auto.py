0ca7b6513c646f671a3f209ab0647bed05a9f1c6
Anikett77 <aniketchaturvedi83@gmail.com>
2026-01-19 20:02:32 +0530

    Automation of youtube shorts

--- Face Detection/auto.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-
-import time
-driver = webdriver.Chrome()
-driver.get("https://www.google.com/")
-time.sleep(4)
-driver.quit()
---
+++ Face Detection/auto_2.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+driver = webdriver.Chrome()
+wait = WebDriverWait(driver,20)
+driver.get("https://www.youtube.com/")
+# short = driver.find_element(By.XPATH, '//*[@title="Shorts"]')
+# shorts = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@title="Shorts"]'))) 
+# shorts.click()
+# time.sleep(5)
+try:
+    shorts = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@title="Shorts"]'))) 
+    shorts.click()
+    time.sleep(5)
+    search_box = driver.find_element(By.NAME,"search_query")
+    search_query = "techsimplus learning"
+    search_box.send_keys(search_query)
+    search_box.send_keys(Keys.RETURN)
+    time.sleep(10)
+    first_short = driver.find_element(By.XPATH, '//*[@id="contents"]/grid-shelf-view-model[1]/div[1]/div')
+    first_short.click()
+    time.sleep(4)
+    while True:
+        error = 0
+        while True:
+            time.sleep(5)
+            break
+        try:
+            nextBtn = driver.find_element(By.XPATH, '//*[@id="navigation-button-down"]/ytd-button-renderer/yt-button-shape/button/yt-touch-feedback-shape/div[2]')
+            nextBtn.click()
+            time.sleep(5)
+        except:
+            break
+        time.sleep(1)
+except:
+    print("error")
+driver.quit()
